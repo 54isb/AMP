11ec39a9b396e5f0cfef1a9d2b2c7bdba2b41fa3
54isb <46381115+54isb@users.noreply.github.com>
2019-01-05 02:12:29 +0900

    Add files via upload

---
+++ approximate_message_passing_LASSO_Algo0.py
@@ -0,0 +1,80 @@
+#Approximate Message Passing (AMP) for LASSO
+#"How to Design Message Passing Algorithm for Compressed Sensing"
+#David L. Donoho, et al.
+#Code by K. Ishibashi, 2017.
+import numpy as np
+import matplotlib.pyplot as plt
+N = 10000 #number of elements to estimate
+n = 5000 #number of observations (n should be even less than N)
+k = 2 #number of nonzero elements
+delta = n/N #undersampling fraction (or indeterminacy)
+rho = k/n #measure of sparsity
+itermax = 30 #maximum number of iteration
+sig2 = 1e-7 #Noise variance
+lam = sig2 #free parameter for soft-threshold function
+
+#Show simulation setup
+str = """\
+N : {0}, n: {1}, k: {2}
+Undersampling fraction: {3}, Sparsity:{4}
+""".format(N,n,k,delta,rho)
+print(str)
+
+#Calculation of MSE
+def mean_squared_error(y, t):
+    return np.sum((y-t)**2)/np.size(y)
+
+#Soft threshould function
+def eta(u, x):
+    return(np.sign(u)*np.maximum(0,np.absolute(u) - x))
+
+#Derivative of soft threshold function
+def d_eta(u, x):
+    return np.piecewise(u,[np.absolute(u)<x,np.absolute(u)>=x],[0.0,1.0])
+
+#for plot
+#plotx = np.arange(1,itermax+1,1)
+#ploty = np.zeros((itermax,1))
+
+#N signals w/ k nonzero elements
+x0 = np.zeros((N,1)) #generate an array w/ N zeros
+x0[0:k] = np.random.rand(k,1)  #put k nonzero elements
+np.random.shuffle(x0) #shuffle it
+
+#1)Observation matrix (n x N), iid Gaussian w/ mean 0, var 1/n
+A = np.random.normal(0, np.sqrt(1.0/n), (n,N))
+
+#2)Observation matrix (n x N) (Donoho's model)
+#A = 1/np.sqrt(n) * (1 - 2*np.random.randint(0,2,(n,N)))
+
+#n observation signals
+#Added noise vector (n x 1), iid Gaussian w/ mean 0, var sig2
+y = np.dot(A,x0) + np.random.normal(0, np.sqrt(sig2), (n,1))
+
+#Appproximate Message Passing ***************
+#Initialization
+xp = np.zeros((N,1)) #x(0)
+z = y #z(0)
+gamma = 1.0/np.sqrt(n)
+#gamma(0) (depending on channel parameter)
+# Note: This free-parameter significantly affects the resulting performance.
+x = eta(np.dot(A.T,z),gamma+lam) #x(1)= z(0),gamma(0)
+
+#start main algo.
+for i in range(itermax): #iteration count
+    #calculate <\eta^{\prime} (A^* z(t-1) + x(t-1));\gamma(t-1)>
+    m = d_eta(np.dot(A.T,z) + xp,gamma+lam).mean()
+    #Factor update LHS: z(x(t), z(t-1), x(t-1), gamma(t-1))
+    z = y - np.dot(A,x) + np.dot(np.dot(1.0/delta, z),m)
+    #update: gamma(gamma(t-1), z(t-1), x(t-1))
+    gamma = (gamma+lam)/delta * m
+    #hold previous value before update x(t)
+    xp = x
+    #Variable update rule LHS: x(t+1)
+    x = eta(np.dot(A.T,z) + x,gamma+lam)
+    #Plot MSE at every iteration
+    result = "{0}, {1}".format(i,mean_squared_error(x0,x))
+    print(result)
+
+#show result
+print("\nIteration",i,",Resulting MSE:",mean_squared_error(x0,x))
